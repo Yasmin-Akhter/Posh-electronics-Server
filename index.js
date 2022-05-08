640396ce205631fd35d68fbb32490ab156f186e1
Yasmin Akhter <yasminbd89@yahoo.com>
2022-05-08 23:07:21 +0600

    get added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const { ObjectId } = require('mongodb');
+
+
+
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,16 +14,57 @@ app.use(express.json());
 
 
 
-
-const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASS@cluster0.xoif2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xoif2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const productCollection = client.db("warehouse").collection("product");
-    console.log('connected db');
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("warehouse").collection("product");
 
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            console.log(req.params.id);
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result)
+        })
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectID(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result)
+        });
+
+        // app.delete('/user/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await userCollection.deleteOne(query);
+        //     res.send(result);
+
+        // })
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
