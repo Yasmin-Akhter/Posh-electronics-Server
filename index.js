67bd3dec156bcca95ea72ccb0d8c2aafa96415c5
Yasmin Akhter <yasminbd89@yahoo.com>
2022-05-09 19:29:16 +0600

    deploying

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
-
-
-
 const port = process.env.PORT || 5000;
 const app = express();
 
